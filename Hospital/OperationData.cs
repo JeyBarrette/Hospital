6d106beb0f58c859c067279950b923dce84bc760
JeyBarrette <olegpetrow2001@gmail.com>
2024-11-14 00:09:57 +0500

    Unit-тесты

---
+++ Hospital/OperationData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace Hospital
+{
+    public class OperationData
+    {
+        public static bool OperationDataValue(string OperationDescription, int DoctorID, DateTime OperationDate, int PatientID, int OperationResult)
+        {
+            bool isValid = true;
+
+            // Проверка OperationDescription
+            if (string.IsNullOrWhiteSpace(OperationDescription) || (string.IsNullOrEmpty(OperationDescription)))
+            {
+                isValid = false;
+            }
+
+            // Проверка длины OperationDescription
+            if (OperationDescription.Length > 60)
+            {
+                isValid = false;
+            }
+
+            // Проверка PatientID
+            if (PatientID < 0)
+            {
+                isValid = false;
+            }
+
+            // Проверка DoctorID
+            if (DoctorID < 0)
+            {
+                isValid = false;
+            }
+
+            // Проверка OperationResult
+            if (OperationResult != 0 && OperationResult != 1)
+            {
+                isValid = false;
+            }
+
+            // Проверка OperationDate
+            if (OperationDate == DateTime.MinValue || OperationDate == default(DateTime))
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+}
---
+++ HospitalTests/OperationDataTests.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hospital;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Tests
+{
+    [TestClass()]
+    public class OperationDataTests
+    {
+        [TestMethod()]
+        public void OperationDataValue_ValidData_ReturnsTrue()
+        {
+            string OperationDescription = "Новая операция";
+            int DoctorID = 2;
+            DateTime OperationDate = new DateTime(2024, 11, 14);
+            int PatientID = 3;
+            int OperationResult = 0;
+            bool expected = true;
+
+            OperationData operationData = new OperationData();
+            bool actual = OperationData.OperationDataValue(OperationDescription, DoctorID, OperationDate, PatientID, OperationResult);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void OperationDataValue_EmptyOperationDescription_ReturnsFalse()
+        {
+            string OperationDescription = "";
+            int DoctorID = 2;
+            DateTime OperationDate = new DateTime(2024, 11, 14);
+            int PatientID = 3;
+            int OperationResult = 0;
+            bool expected = false;
+
+            OperationData operationData = new OperationData();
+            bool actual = OperationData.OperationDataValue(OperationDescription, DoctorID, OperationDate, PatientID, OperationResult);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void OperationDataValue_WhiteSpacesOperationDescription_ReturnsFalse()
+        {
+            string OperationDescription = "     ";
+            int DoctorID = 2;
+            DateTime OperationDate = new DateTime(2024, 11, 14);
+            int PatientID = 3;
+            int OperationResult = 0;
+            bool expected = false;
+
+            OperationData operationData = new OperationData();
+            bool actual = OperationData.OperationDataValue(OperationDescription, DoctorID, OperationDate, PatientID, OperationResult);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void OperationDataValue_UpperBoundOperationDescription_ReturnsFalse()
+        {
+            string OperationDescription = "ЗАПОЛНЕНИЕЗАПОЛНЕНИЕЗАПОЛНЕНИЕЗАПОЛНЕНИЕЗАПОЛНЕНИЕЗАПОЛНЕНИЕЗ";
+            int DoctorID = 3;
+            DateTime OperationDate = new DateTime(2024, 11, 14);
+            int PatientID = 4;
+            int OperationResult = 1;
+            bool expected = false;
+
+            OperationData operationData = new OperationData();
+            bool actual = OperationData.OperationDataValue(OperationDescription, DoctorID, OperationDate, PatientID, OperationResult);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void OperationDataValue_UpperBoundOperationDescription_ReturnsTrue()
+        {
+            string OperationDescription = "ЗАПОЛНЕНИЕЗАПОЛНЕНИЕЗАПОЛНЕНИЕЗАПОЛНЕНИЕЗАПОЛНЕНИЕЗАПОЛНЕНИЕ";
+            int DoctorID = 3;
+            DateTime OperationDate = new DateTime(2024, 11, 14);
+            int PatientID = 4;
+            int OperationResult = 1;
+            bool expected = true;
+
+            OperationData operationData = new OperationData();
+            bool actual = OperationData.OperationDataValue(OperationDescription, DoctorID, OperationDate, PatientID, OperationResult);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void OperationDataValue_NegativePatientID_ReturnsFalse()
+        {
+            string OperationDescription = "Тест операция";
+            int DoctorID = 6;
+            DateTime OperationDate = new DateTime(2024, 11, 14);
+            int PatientID = -1;
+            int OperationResult = 0;
+            bool expected = false;
+
+            OperationData operationData = new OperationData();
+            bool actual = OperationData.OperationDataValue(OperationDescription, DoctorID, OperationDate, PatientID, OperationResult);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void OperationDataValue_NegativeDoctorID_ReturnsFalse()
+        {
+            string OperationDescription = "Тест Тест";
+            int DoctorID = -1;
+            DateTime OperationDate = new DateTime(2024, 11, 14);
+            int PatientID = 5;
+            int OperationResult = 0;
+            bool expected = false;
+
+            OperationData operationData = new OperationData();
+            bool actual = OperationData.OperationDataValue(OperationDescription, DoctorID, OperationDate, PatientID, OperationResult);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void OperationDataValue_InvalidOperationResult_ReturnsFalse()
+        {
+            string OperationDescription = "ТестыТестов";
+            int DoctorID = 7;
+            DateTime OperationDate = new DateTime(2024, 11, 14);
+            int PatientID = 3;
+            int OperationResult = 10;
+            bool expected = false;
+
+            OperationData operationData = new OperationData();
+            bool actual = OperationData.OperationDataValue(OperationDescription, DoctorID, OperationDate, PatientID, OperationResult);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void OperationDataValue_ValidDate_ReturnsTrue()
+        {
+            string OperationDescription = "ТестыТестовТестов";
+            int DoctorID = 2;
+            DateTime OperationDate = new DateTime(2024, 11, 14);
+            int PatientID = 2;
+            int OperationResult = 0;
+            bool expected = true;
+
+            OperationData operationData = new OperationData();
+            bool actual = OperationData.OperationDataValue(OperationDescription, DoctorID, OperationDate, PatientID, OperationResult);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void OperationDataValue_InvalidDate_ReturnsFalse()
+        {
+            string OperationDescription = "Тестыыыы";
+            int DoctorID = 3;
+            DateTime OperationDate = DateTime.MinValue;
+            int PatientID = 1;
+            int OperationResult = 1;
+            bool expected = false;
+
+            OperationData operationData = new OperationData();
+            bool actual = OperationData.OperationDataValue(OperationDescription, DoctorID, OperationDate, PatientID, OperationResult);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
